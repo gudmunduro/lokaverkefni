37b8c00937afe3587a4e02781a86a3770ad4f8a5
gudmunduroh <gudmundurha203@nemi.tskoli.is>
2017-11-20 14:48:59 +0000

    Breytingar

---
+++ api.py
@@ -0,0 +1,6 @@
+from bottle import *
+
+
+@route("/a")
+def main():
+    return " b"
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # Lokaverkefni vef hluti - Guðmundur Óli og Helgi Steinarr
-from bottle import *
+from api import *
+
+
+@route("/")
+def main():
+    return ""
+
+if __name__ == '__main__':
+    run()
