f293930ff05531098dc31f50df0c665bc5ed2b08
Guðmundur Óli <gudmundurha203@nemi.tskoli.is>
2017-11-23 10:59:53 +0000

    Lagað animation

--- web/incl/script/TabController.js
+++ web/incl/script/TabController.js
@@ -10,7 +10,7 @@ var TabController = {
 
     set currentTab(index) {
         if (index == TabController.currentTabIndex) return;
-        TabController.getTabById(index).style.transform = "translateX(" + (index > TabController.currentTabIndex) ? "100" : "-100" + "vw)";
+        TabController.getTabById(index).style.transform = "translateX(" + ((index > TabController.currentTabIndex) ? "100" : "-100") + "vw)";
         TabController.getTabById(index).style.display = "block";
         setTimeout(function () {
             TabController.getTabById(TabController.currentTabIndex).style.transform = "translateX(" + ((index > TabController.currentTabIndex) ? "-100" : "100") + "vw)";
@@ -19,6 +19,6 @@ var TabController = {
                 TabController.getTabById(TabController.currentTabIndex).style.display = "none";
                 TabController.currentTabIndex = index;
             }, 300)
-        }, 30)
+        }, 50)
     }
 }
